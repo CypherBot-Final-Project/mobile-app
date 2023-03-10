a2772af1c7e65ee6b683a9f3f3c242a0b3ad9da2
Sahanon Phisetpakasit <pingotakulml@gmail.com>
2023-03-10 16:56:25 +0700

    feat: able to fetch market data

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,81 @@
+import 'dart:async';
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
-void main() {
-  runApp(const MyApp());
+Future<Asset> fetchAsset() async {
+  final response = await http
+      .get(Uri.parse('https://api.coingecko.com/api/v3/coins/markets?vs_currency=thb'));
+
+  if (response.statusCode == 200) {
+    // If the server did return a 200 OK response,
+    // then parse the JSON.
+    Iterable l = json.decode(response.body);
+    return Asset.fromJson(l.first);
+  } else {
+    // If the server did not return a 200 OK response,
+    // then throw an exception.
+    throw Exception('Failed to load album');
+  }
 }
 
-class MyApp extends StatelessWidget {
+class Asset {
+  String name;
+  String image;
+  late int marketCap;
+  late int totalVolume; 
+  late double circulatingSupply; 
+  late int high24hr;
+  late int low24hr;
+  late int marketCapRank;
+  late double priceChange24hr;
+
+  Asset({
+      required this.name,
+      required this.image,
+      required this.marketCap,
+      required this.totalVolume, 
+      required this.circulatingSupply, 
+      required this.high24hr,
+      required this.low24hr,
+      required this.marketCapRank,
+      required this.priceChange24hr,
+  });
+  factory Asset.fromJson(Map<String, dynamic> json){
+    return Asset(
+      name: json['name'],
+      image: json['image'],
+      marketCap: json['market_cap'],
+      totalVolume: json['total_volume'],
+      circulatingSupply: json['circulating_supply'],
+      high24hr: json['high_24h'],
+      low24hr: json['low_24h'],
+      marketCapRank: json['market_cap_rank'],
+      priceChange24hr: json['price_change_24h'],
+    );
+  }
+  
+}
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  late Future<Asset> futureAsset;
+
+  @override
+  void initState() {
+    super.initState();
+    futureAsset = fetchAsset();
+  }
+
+    @override
   Widget build(BuildContext context) {
     Widget titleSection = Row(
       children: [
@@ -37,6 +103,7 @@ class MyApp extends StatelessWidget {
         )
       ],
     );
+
     Color color = const Color.fromARGB(255, 14, 34, 53);
     return MaterialApp(
         theme: ThemeData(fontFamily: "Oxygen"),
@@ -59,7 +126,19 @@ class MyApp extends StatelessWidget {
         ),
         backgroundColor: color,
         body: TabBarView(
-          children: [_buildCard(), const Center(child: Icon(Icons.construction, size: 200, color: Colors.white,))],
+          children: [_buildCard(), Center(child: FutureBuilder<Asset>(
+            future: futureAsset,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return Text(snapshot.data!.name);
+              } else if (snapshot.hasError) {
+                return Text('${snapshot.error}');
+              }
+
+              // By default, show a loading spinner.
+              return const CircularProgressIndicator();
+            },
+          ),)],
         ),
       ),
     ));
@@ -85,16 +164,16 @@ class MyApp extends StatelessWidget {
           ),
         ),
         Card(
-          color: Color.fromARGB(255, 73, 109, 128),
+          color: const Color.fromARGB(255, 73, 109, 128),
           shadowColor: Colors.white,
           child: ExpansionTile(
             leading: Image.network("https://assets.coingecko.com/coins/images/279/large/ethereum.png?1595348880", height: 30, width: 30,),
-            title: Text(
+            title: const Text(
               'Etheruem',
               style: TextStyle(color: Colors.white),
             ),
             iconColor: Colors.white,
-            children: <Widget>[
+            children: const <Widget>[
               Text('Big Bang'),
               Text('Birth of the Sun'),
               Text('Earth is Born'),
