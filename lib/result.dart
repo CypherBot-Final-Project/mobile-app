6ba5794e58fb6478d57e0f4d94a58a4d372c79a1
Sahanon Phisetpakasit <pingotakulml@gmail.com>
2023-03-15 14:32:56 +0700

    style: initial page for result and dashboard

---
+++ lib/result.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Result extends StatefulWidget {
+  final String money;
+  final String provider;
+  const Result(this.money, this.provider, {super.key});
+
+  @override 
+  State<Result> createState() => _ResultState();
+}
+
+class _ResultState extends State<Result>{
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(home: Scaffold(appBar: AppBar(title: Text("${widget.money}, ${widget.provider}"))),);
+  }
+}
+
